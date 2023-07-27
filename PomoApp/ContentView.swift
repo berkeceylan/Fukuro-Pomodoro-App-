4a9a2d49d1d7c40031b1ce45b58e88e63a1d752e
Berke Ceylan <berkeceylan12@hotmail.com>
2023-07-27 22:36:12 +0300

    Version 0.2
    Break feature and break timer display added

--- PomoApp/ContentView.swift
+++ PomoApp/ContentView.swift
@@ -8,57 +8,106 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var countDownInMinutes = 25 * 60
-    @State var userInput = 25
-    @State var timerRunning = false
+    @State var sessionInMinutes = 25 * 60
+    @State var breakInMinutes = 5 * 60
+    @State var sessionTime = 25
+    @State var breakTime = 5
+    @State var isSession = false
+    @State var isBreak = false
+    
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
-    var minutes: Int{
-        return countDownInMinutes/60
+    var sessionMinutes: Int {
+        return sessionInMinutes / 60
     }
     
-    var seconds: Int {
-        return countDownInMinutes%60
+    var sessionSeconds: Int {
+        return sessionInMinutes % 60
+    }
+    
+    var breakMinutes: Int {
+        return breakInMinutes / 60
+    }
+    
+    var breakSeconds: Int {
+        return breakInMinutes % 60
     }
     
     var body: some View {
-        VStack{
-            Text("\(minutes):\(seconds < 10 ? "0" : "")\(seconds)")
-                .onReceive(timer){ _ in
-                    if countDownInMinutes > 0 && timerRunning == true{
-                        countDownInMinutes -= 1
-                    }
-                    else{
-                        timerRunning = false
-                    }
-                }
-                .font(.system(size: 80, weight: .bold))
-                .opacity(0.80)
+        VStack {
             
-            Stepper(value: $userInput, in: 1...60, step: 1){
-                Text("Set Countdown")
+                
+            VStack(alignment: .center) {
+                // Display for Session Time
+                Text("Session Time")
+                    .font(.headline)
+                Text("\(sessionMinutes):\(sessionSeconds < 10 ? "0" : "")\(sessionSeconds)")
+                    .font(.system(size: 80, weight: .bold))
+                    .opacity(0.80)
             }
             .padding()
-            .onChange(of: userInput){
-                munitesValue in
-                countDownInMinutes = munitesValue * 60
-                
+            
+            VStack(alignment: .center) {
+                // Display for Break Time
+                Text("Break Time")
+                    .font(.headline)
+                Text("\(breakMinutes):\(breakSeconds < 10 ? "0" : "")\(breakSeconds)")
+                    .font(.system(size: 40, weight: .bold))
+                    .opacity(0.80)
+            }
+            .padding()
+                    
+            
+            
+            Stepper(value: $sessionTime, in: 1...60, step: 1) {
+                Text("Set Session Time")
+            }
+            .padding()
+            .onChange(of: sessionTime) { sessionValue in
+                sessionInMinutes = sessionValue * 60
             }
             
-            HStack(spacing: 30){
-                Button("Start"){
-                    timerRunning = true
+            Stepper(value: $breakTime, in: 1...60, step: 1) {
+                Text("Set Break Time")
+            }
+            .padding()
+            .onChange(of: breakTime) { breakValue in
+                breakInMinutes = breakValue * 60
+            }
+            
+            HStack(spacing: 30) {
+                Button("Start") {
+                    isSession = true
                 }
                 
-                Button("Reset"){
-                    countDownInMinutes = 25 * 60
-                    userInput = 25
-                    timerRunning = false
-                }.foregroundColor(.red)
+                Button("Reset") {
+                    sessionInMinutes = sessionTime * 60
+                    breakInMinutes = breakTime * 60
+                    isSession = false
+                    isBreak = false
+                }
+                .foregroundColor(.red)
             }
-            
         }
-        
+        .onReceive(timer) { _ in
+            if isSession {
+                if sessionInMinutes > 0 {
+                    sessionInMinutes -= 1
+                } else {
+                    isSession = false
+                    isBreak = true
+                    breakInMinutes = breakTime * 60
+                }
+            } else if isBreak {
+                if breakInMinutes > 0 {
+                    breakInMinutes -= 1
+                } else {
+                    isSession = true
+                    isBreak = false
+                    sessionInMinutes = sessionTime * 60
+                }
+            }
+        }
     }
 }
 
